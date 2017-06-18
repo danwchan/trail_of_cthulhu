0ec2833be6ff66f1aac06c8bdd16dedf172a8eda
danwchan <danwchan@gmail.com>
2017-06-18 15:22:52 -0500

    collapsing the admin site into distinct pages which reference multiple tables

--- mythos_website_upgrade/birthcharacter/admin.py
+++ mythos_website_upgrade/birthcharacter/admin.py
@@ -8,25 +8,42 @@ from .models import AbilityList, AbilityExamples, OccupationList, DriveList, Dri
 #    'ability'
 #    ]
 
-# Some ModelAdmin classes to bind it all together
+# Inline projects to build the editing forms
+class AbilityInLine(admin.TabularInline):
+    model = AssociatedOccuAbil
+    fk_name = 'associated_occupations'
+    extra = 0
+
+# ModelAdmin classes to bind it all together representing editing forms
 
 class AbilityAdmin(admin.ModelAdmin):
-    list_display = ('ability',)
-
+    list_display = ['ability', 'purist', 'pulp', 'rating']
+    
 class OccupationAdmin(admin.ModelAdmin):
-    list_display = ('occupation',)
+    list_display = ['occupation', 'purist', 'pulp', 'rating']
+    search_fields = ['occupationlist__occupation']
+    inlines = [
+        AbilityInLine
+    ]
+    
+    def _abilitylist(self, obj):
+        return obj.abilitylist.all().count()
 
 class DriveAdmin(admin.ModelAdmin):
-    list_display = ('drive',)
+    list_display = ['drive', 'purist', 'pulp', 'rating']
 
+class AbilityExAdmin(admin.ModelAdmin):
+    list_display = ['get_ability', 'purist', 'pulp', 'rating']
     
+    def get_ability(self, obj):
+        return obj.abilitylist.ability
+    get_ability.short_description = 'Ability'
+
 # Register your models here.
 
 admin.site.register(AbilityList, AbilityAdmin)
-admin.site.register(AbilityExamples)
+admin.site.register(AbilityExamples, AbilityExAdmin)
 admin.site.register(OccupationList, OccupationAdmin)
 admin.site.register(SpecialList)
 admin.site.register(DriveList, DriveAdmin)
 admin.site.register(DriveExamples)
-admin.site.register(AssociatedOccuDrive)
-admin.site.register(AssociatedOccuAbil)
