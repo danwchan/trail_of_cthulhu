b07f09b3416c50a2a6b1c39b37cb867027118780
danwchan <danwchan@gmail.com>
2017-06-17 19:50:48 -0500

    finalized character creation reference models before data input for testing

--- mythos_website_upgrade/birthcharacter/admin.py
+++ mythos_website_upgrade/birthcharacter/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-from .models import AbilityList, AbilityExamples, OccupationList, DriveList, DriveExamples, AssociatedOccuDrive, AssociatedOccuAbil
+from .models import AbilityList, AbilityExamples, OccupationList, DriveList, DriveExamples, AssociatedOccuDrive, AssociatedOccuAbil, SpecialList
 
+# Primary keys you care about
+#primary_keys = [
+#    'occupation',
+#    'drive',
+#    'ability'
+#    ]
+
+# Some ModelAdmin classes to bind it all together
+
+class AbilityAdmin(admin.ModelAdmin):
+    list_display = ('ability',)
+
+class OccupationAdmin(admin.ModelAdmin):
+    list_display = ('occupation',)
+
+class DriveAdmin(admin.ModelAdmin):
+    list_display = ('drive',)
+
+    
 # Register your models here.
 
-admin.site.register(AbilityList)
+admin.site.register(AbilityList, AbilityAdmin)
 admin.site.register(AbilityExamples)
-admin.site.register(OccupationList)
-admin.site.register(DriveList)
+admin.site.register(OccupationList, OccupationAdmin)
+admin.site.register(SpecialList)
+admin.site.register(DriveList, DriveAdmin)
 admin.site.register(DriveExamples)
 admin.site.register(AssociatedOccuDrive)
 admin.site.register(AssociatedOccuAbil)
--- mythos_website_upgrade/birthcharacter/models.py
+++ mythos_website_upgrade/birthcharacter/models.py
@@ -4,8 +4,9 @@ from randomslugfield import RandomSlugField
 class RevisionStatus(models.Model):
     created = models.DateTimeField('the date this record was created',auto_now=True)
     updated = models.DateTimeField('the last point this record was updated',auto_now_add=True)
-    original = models.BooleanField('is this record from the Trail of Cthulhu or has it been reworked for the new modern day setting?')
-    comments = models.TextField('comments which have been associated with this record')
+    original = models.BooleanField('TRUE: this record from the Trail of Cthulhu FALSE: it been reworked for the new modern day setting')
+    comments = models.TextField('comments which have been associated with this record',
+        blank = True)
     RATING_CHOICES = (
         (1, 'works well'),
         (2, 'few complaints'),
@@ -14,15 +15,25 @@ class RevisionStatus(models.Model):
         (5, 'might be broken')
     )
     rating = models.IntegerField('a rating number where 1 is the best and 5 requires a rewite',default=3)
-    purist = models.BooleanField('is this relevant for purist settings')
-    pulp = models.BooleanField('is this relevant for pulp settings')
+    purist = models.BooleanField('relevant for purist settings')
+    pulp = models.BooleanField('relevant for pulp settings')
+    
+    def __str__(self):
+        return str(self.pk)    
 
 class OccupationList(RevisionStatus):
     occupation = models.CharField('the name of the occupation',max_length=50, primary_key=True)
     credit_lower = models.IntegerField('the lower limit of the credit rating')
     credit_upper = models.IntegerField('the upper limit of the credit rating')
-    special = models.TextField('the special benefits rendered onto a character of this occupation')
     description = models.TextField('what is this occupation?')
+    
+class SpecialList(RevisionStatus):
+    occupation = models.ForeignKey(
+        OccupationList,
+        default = 'unassigned_special',
+        on_delete = models.SET_DEFAULT
+    )
+    special = models.TextField('the special benefits rendered onto a character of this occupation')
 
 class DriveList(RevisionStatus):
     drive = models.CharField('the name of the drive',max_length=25, primary_key=True)
@@ -64,7 +75,7 @@ class AbilityList(RevisionStatus):
     )
     investigative_type = models.CharField(
         max_length =1,
-        choices = MAJOR_TYPE_CHOICES
+        choices = INVESTIGATIVE_TYPE_CHOICES
     )
     description = models.TextField()
     
@@ -101,7 +112,14 @@ class AssociatedOccuAbil(RevisionStatus):
         OccupationList,
         default = 'none',
         on_delete=models.CASCADE
-    )    
+    )
+    conditional_options = models.CharField('any restrictions or modifiers',
+        max_length = 75,
+        blank = True)
+    #for example conditional_options may restrict occupational abilities to a sub class of occupation, or they may specifiy choosing from a pool which should be represented as choose X from a pool of Y: X
+    ability_options = models.IntegerField('this association is for a choice from several options',
+        default = False
+    )
 
 class BirthForm(models.Model):
     birthcode = RandomSlugField(length=7)
