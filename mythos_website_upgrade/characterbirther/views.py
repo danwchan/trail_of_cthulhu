862ae19f472aa906decda3dd8a742fcc947edf1e
danwchan <danwchan@gmail.com>
2017-06-22 09:29:41 -0500

    update the views based on tutorial, ready to start makeing the page

--- mythos_website_upgrade/characterbirther/views.py
+++ mythos_website_upgrade/characterbirther/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from birthcharacter.models import OccupationList
 # Create your views here.
 
 def make_investigator(request):
-    return render(request, 'characterbirther/make_investigator.html', {})
+    occupations = OccupationList.objects.all()
+    return render(request, 'characterbirther/make_investigator.html', {'occupations' : occupations})
