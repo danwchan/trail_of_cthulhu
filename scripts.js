f0b1a558adc1a1bba6383c9fdedc8d7e74e1c9e3
danwchan <danwchan@gmail.com>
2017-06-30 19:34:21 -0500

    built the general format of the character cart button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,62 @@
+// polyfill to have classList work with IE8-9 Source: https://gist.github.com/k-gun/c2ea7c49edf7b757fe9561ba37cb19ca
+;(function() {
+    // helpers
+    var regExp = function(name) {
+        return new RegExp('(^| )'+ name +'( |$)');
+    };
+    var forEach = function(list, fn, scope) {
+        for (var i = 0; i < list.length; i++) {
+            fn.call(scope, list[i]);
+        }
+    };
+
+    // class list object with basic methods
+    function ClassList(element) {
+        this.element = element;
+    }
+
+    ClassList.prototype = {
+        add: function() {
+            forEach(arguments, function(name) {
+                if (!this.contains(name)) {
+                    this.element.className += this.element.className.length > 0 ? ' ' + name : name;
+                }
+            }, this);
+        },
+        remove: function() {
+            forEach(arguments, function(name) {
+                this.element.className =
+                    this.element.className.replace(regExp(name), '');
+            }, this);
+        },
+        toggle: function(name) {
+            return this.contains(name) 
+                ? (this.remove(name), false) : (this.add(name), true);
+        },
+        contains: function(name) {
+            return regExp(name).test(this.element.className);
+        },
+        // bonus..
+        replace: function(oldName, newName) {
+            this.remove(oldName), this.add(newName);
+        }
+    };
+
+    // IE8/9, Safari
+    if (!('classList' in Element.prototype)) {
+        Object.defineProperty(Element.prototype, 'classList', {
+            get: function() {
+                return new ClassList(this);
+            }
+        });
+    }
+
+    // replace() support for others
+    if (window.DOMTokenList && DOMTokenList.prototype.replace == null) {
+        DOMTokenList.prototype.replace = ClassList.prototype.replace;
+    }
+})();
+
 // Used to toggle the menu on small screens when clicking on the menu button, want to make this an accordian someday
 function toggleShow(id) {
     var x = document.getElementById(id);
@@ -19,4 +78,12 @@ function myAccordian(id) {
         x.previousElementSibling.className = 
         x.previousElementSibling.className.replace(" w3-deepred", "");
     }
+}
+
+//used to toggle right side column "sidebar" which 
+function toggleSidecol() {
+  document.getElementById("main").classList.toggle("w3-half");
+  document.getElementById("sidebar").classList.toggle("w3-half");
+  document.getElementById("sidebar").classList.toggle("w3-show");
+  document.getElementById("floatbtn").classList.toggle("w3-hide");
 }
